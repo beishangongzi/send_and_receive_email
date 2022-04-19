df2c0833af4495bd4965902b2ecefd0b700f9530
Andy Z Wright <zhangruibin021@gmail.com>
2022-04-19 23:23:12 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# create by andy at 2022/4/19
+# reference:
+
+# Email settings
+SEND_EMAIL_HOST = 'smtp.126.com'
+SEND_EMAIL_HOST_USER = 'andyelizabeth021@126.com'
+SEND_EMAIL_HOST_PASSWORD = 'you password' #past the key or password app here
+SEND_EMAIL_PORT = 25
+SEND_EMAIL_USE_TLS = True
+SEND_DEFAULT_FROM_EMAIL = 'andyelizabeth021@126.com'
+# KOPSYFUAWFZUOJLP
+
+# Receive settings
+RECEIVE_EMAIL_HOST = 'pop.163.com'
+RECEIVE_EMAIL_HOST_USER = 'andyzwright021@163.com'
+RECEIVE_EMAIL_HOST_PASSWORD = 'your another password' #past the key or password app here
+RECEIVE_EMAIL_PORT = 995
+RECEIVE_EMAIL_USE_TLS = True
+RECEIVE_DEFAULT_FROM_EMAIL = 'andyelizabeth021@126.com'
+
+if __name__ == '__main__':
+    pass
